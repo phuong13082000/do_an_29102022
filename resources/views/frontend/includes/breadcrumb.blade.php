c63aec04e717ccedec09ea73c835d9d111f94e65
HoangPhuong <hoangphuong0813@gmail.com>
2022-11-11 02:33:57 +0700

    11112022

---
+++ resources/views/frontend/includes/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light mt-3">
+    <div class="container-fluid">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
+            </ol>
+        </nav>
+    </div>
+</nav>
